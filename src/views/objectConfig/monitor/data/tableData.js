ac6e9c31b5fa25d15d94b614a1a45c87323c1bb7
jiang <957525929@qq.com>
2022-01-08 18:01:45 +0800

    页面修改

--- src/views/objectConfig/monitor/data/tableData.js
+++ src/views/objectConfig/monitor/data/tableData.js
@@ -1,36 +1,54 @@
-export const columns = [
-  {
-    title: '序号',
-    dataIndex: 'num',
-    scopedSlots: { customRender: 'num' }
-  },
-  {
-    title: '名称',
-    dataIndex: 'name'
-  },
-  // {
-  //   title: '英文名称',
-  //   dataIndex: 'eName'
-  // },
-  {
-    title: '所属场景',
-    dataIndex: 'scene',
-    scopedSlots: { customRender: 'scene' }
-  },
-  {
-    title: '更新时间',
-    dataIndex: 'updateTime',
-    scopedSlots: { customRender: 'updateTime' }
-  },
-  {
-    title: '描述',
-    dataIndex: 'description',
-    scopedSlots: { customRender: 'description' }
-  },
-  {
-    title: '操作',
-    key: 'action',
-    scopedSlots: { customRender: 'action' }
-  }
-]
-export const tableData = []
+export const monitor = {
+  columns: [
+    {
+      title: '序号',
+      dataIndex: 'num',
+      scopedSlots: { customRender: 'num' }
+    },
+    {
+      title: '名称',
+      dataIndex: 'name'
+    },
+    // {
+    //   title: '英文名称',
+    //   dataIndex: 'eName'
+    // },
+    {
+      title: '所属场景',
+      dataIndex: 'scene',
+      scopedSlots: { customRender: 'scene' }
+    },
+    {
+      title: '更新时间',
+      dataIndex: 'updateTime',
+      scopedSlots: { customRender: 'updateTime' }
+    },
+    {
+      title: '描述',
+      dataIndex: 'description',
+      scopedSlots: { customRender: 'description' }
+    },
+    {
+      title: '操作',
+      key: 'action',
+      scopedSlots: { customRender: 'action' }
+    }
+  ],
+  tableData: [],
+  form_item: [
+    {
+      type: 'input',
+      label: '名称',
+      name: 'name'
+    },
+    {
+      type: 'picker',
+      label: '时间',
+      format: 'YYYY-MM-DD',
+      startTimeLabel: '开始时间',
+      endTimeLabel: '结束时间',
+      startTime: 'startTime',
+      endTime: 'endTime'
+    }
+  ]
+}
--- src/views/objectConfig/monitor/index.vue
+++ src/views/objectConfig/monitor/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
     <a-card>
-      <Search ref="search" :sceneOption="sceneOption" @search="searchData" />
+      <!-- <Search ref="search" :sceneOption="sceneOption" @search="searchData" /> -->
+      <Search
+        ref="search"
+        :formItem="form_item"
+        :formHandler="form_handler"
+        :formData="queryParam"
+        :formLayout="formLayout"
+      />
     </a-card>
     <a-card>
-      <Table ref="table" :columns="columns" :tableData="tableData" :sceneOption="sceneOption" @updateData="updateData">
+      <Table ref="table" :columns="columns" :tableData="dataSource" :sceneOption="sceneOption" @updateData="updateData">
       </Table>
     </a-card>
   </div>
@@ -12,60 +19,74 @@
 
 <script>
 import Table from './components/table'
-import Search from './components/search'
-import { columns, tableData } from './data/tableData'
+import Search from '@/components/form'
+// import Search from './components/search'
+import { monitor } from './data/tableData'
 import { getSceneItemList, getSceneList } from '@/api/scene'
-
+import { JeecgListMixin } from '@/mixins/JeecgListMixin'
 import moment from 'moment'
 export default {
+  mixins: [JeecgListMixin],
   components: {
     Table,
     Search
   },
   data() {
     return {
-      columns,
-      tableData,
       sceneOption: [],
-      state: {
+      queryParam: {
         startTime: '',
         endTime: '',
         name: '',
         sceneId: ''
+      },
+      ...monitor,
+      formLayout: 'inline',
+      form_handler: [
+        {
+          label: '查询',
+          key: 'submit',
+          icon: 'search',
+          loading: this.loading,
+          handler: () => this.searchData()
+        }
+      ],
+      url: {
+        list: '/programmerweb/SceneItemController/getSceneItemList'
       }
     }
   },
   mounted() {
-    this.loadData()
+    // this.loadData()
   },
   methods: {
-    async loadData() {
-      this.$refs.table.loading_tab = true
+    async loadSenceData() {
+      // this.$refs.table.loading_tab = true
       let params = {}
-      let data1 = this.state
+      let data1 = this.queryParam
       // 过滤空数据
       for (let key in data1) {
         if (data1[key]) {
           params[key] = data1[key]
         }
       }
-      console.log(params, 'params')
-      let res = await getSceneItemList(params)
+      // console.log(params, 'params')
+      // let res = await getSceneItemList(params)
       let data = await getSceneList()
       this.sceneOption = this.handlerOption(data.data)
-      if (res && this.sceneOption) {
-        for (let key in res.data) {
+      if (this.dataSource && this.sceneOption) {
+        for (let key in this.dataSource) {
           for (let v in this.sceneOption) {
-            if (res.data[key].sceneId == this.sceneOption[v].key) {
-              res.data[key].scene = this.sceneOption[v].value
+            if (this.dataSource[key].sceneId == this.sceneOption[v].key) {
+              this.dataSource[key].scene = this.sceneOption[v].value
             }
           }
         }
       }
-      console.log(res.data)
-      this.tableData = res.data
-      this.$refs.table.loading_tab = false
-      this.$refs.search.loading_but = false
+      // console.log(res.data)
+      // this.tableData = res.data
+      // this.$refs.table.loading_tab = false
+      // this.$refs.search.loading_but = false
     },
     updateData() {
       this.loadData()
@@ -80,10 +101,10 @@ export default {
     },
     searchData(value) {
       console.log('value', value)
-      this.state.startTime = value.startTime
-      this.state.endTime = value.endTime
-      this.state.name = value.name
-      this.state.sceneId = value.sceneId
+      this.queryParam.startTime = value.startTime
+      this.queryParam.endTime = value.endTime
+      this.queryParam.name = value.name
+      this.queryParam.sceneId = value.sceneId
       this.$refs.search.loading_but = true
       // if (value.sceneId == '') {
       //   delete data.sceneId
